5e67a703ef6c0cf690ea3111bebab2cb498981ff
keily Lopez <klopezh16@miumg.edu.gt>
2025-05-16 23:14:59 -0600

    potsgrest

--- Models/Post.cs
+++ Models/Post.cs
@@ -3,8 +3,8 @@ namespace CodeFirstAPI.Models
     public class Post
     {
         public int Id { get; set; }  // primary key
-        public string Title { get; set; }
-        public string Content { get; set; }
+        public  string?  Title { get; set; }
+        public  string? Content { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
